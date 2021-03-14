e6918c977cebe872c300bbe8ee075d915248c866
geospiza <geospiza@protonmail.com>
2021-03-14 01:11:36 -0800

    Add histogram off weapon attack

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,34 @@
   import Character from "./components/Character.svelte";
   import Mob from "./components/Mob.svelte";
   import DamageTaken from "./components/DamageTaken.svelte";
+  import Plot from "./components/Plot.svelte";
 
   let mob = {};
+  let dealWeaponDamage;
+  let dealMagicDamage;
+
+  function rand(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  function transform(data) {
+    return [
+      {
+        x: data,
+        type: "histogram",
+      },
+    ];
+  }
+
+  let samples = 10000;
+  let weapon_damage = [];
+  function sample() {
+    weapon_damage = [];
+    for (let i = 0; i < samples; i++) {
+      weapon_damage.push(dealWeaponDamage(rand(0.008, 0.0085)));
+    }
+    weapon_damage = [...weapon_damage];
+  }
 </script>
 
 <main>
@@ -19,7 +45,23 @@
       <Mob bind:this={mob} />
     </div>
     <div class="col-sm">
-      <DamageTaken {mob} />
+      <DamageTaken {mob} bind:dealWeaponDamage bind:dealMagicDamage />
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <h2>Plots</h2>
+      <button on:click={sample}>Sample and Plot</button>
+
+      {#if weapon_damage.length}
+        <Plot
+          data={weapon_damage}
+          {transform}
+          layout={{
+            title: `histogram of weapon damage taken (n=${weapon_damage.length})`,
+          }}
+        />
+      {/if}
     </div>
   </div>
 </main>
---
+++ src/components/Plot.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { merge } from "lodash";
+
+  export let data;
+  export let transform = (res) => res;
+  export let layout = {};
+
+  let plotElement;
+
+  $: plotElement &&
+    data &&
+    layout &&
+    Plotly.newPlot(
+      plotElement,
+      transform(data),
+      merge(
+        {
+          margin: {
+            l: 50,
+            r: 0,
+            b: 50,
+          },
+        },
+        layout
+      ),
+      { responsive: true }
+    );
+</script>
+
+<div bind:this={plotElement} />
