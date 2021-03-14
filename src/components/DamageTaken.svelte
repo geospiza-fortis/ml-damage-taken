da9688886b9423bb3894cae9a596f34e1540c282
geospiza <geospiza@protonmail.com>
2021-03-14 00:10:08 -0800

    Move damage taken into a component

---
+++ src/components/DamageTaken.svelte
@@ -0,0 +1,81 @@
+<script>
+  import Table from "./Table.svelte";
+  import pddData from "../pdd.json";
+
+  import {
+    job,
+    level,
+    avoid,
+    wdef,
+    mdef,
+    str,
+    dex,
+    luk,
+    int,
+  } from "../store.js";
+
+  export let mob = {};
+  export let dealWeaponDamage = () => 0;
+  export let dealMagicDamage = () => 0;
+
+  function clip(value, min, max) {
+    return Math.min(Math.max(value, min), max);
+  }
+
+  function hitPercentage(job, dodge) {
+    return job == "thief"
+      ? clip(1 - dodge, 0.05, 0.95)
+      : clip(1 - dodge, 0.02, 0.8);
+  }
+
+  // avoidability specific to a mob
+  $: avoid_mob = $level >= mob.level ? $avoid : (mob.level - $level) / 2;
+  $: dodge = avoid_mob / (4.5 * mob.acc);
+  $: hit = hitPercentage($job, dodge);
+  $: mdodge = 10 / 9 - mob.acc / (0.9 * avoid_mob);
+  $: mhit = hitPercentage($job, mdodge);
+  $: _pdd = pddData
+    .filter((row) => row.job == $job)
+    .reverse()
+    .find((row) => $level >= row.level);
+  $: pdd = _pdd && !_pdd.length ? _pdd.pdd : 0;
+
+  $: d = $level >= mob.level ? 13 / (13 + $level - mob.level) : 1.3;
+  // TODO: function of str/dex/int/luk
+  $: c =
+    ($job == "warrior"
+      ? $str / 2800 + $dex / 3200
+      : $str / 2000 + $dex / 2800) +
+    $int / 7200 +
+    $luk / 3200;
+  $: b =
+    $wdef >= pdd
+      ? (c * 28) / 45 + ($level * 7) / 13000 + 0.196
+      : d * (c + $level / 550 + 0.28);
+  $: a = c + 0.28;
+
+  // variance should be rand(0.008, 0.0085), presumably uniform
+  $: dealWeaponDamage = (variance) =>
+    Math.pow(mob.watt, 2) * variance - $wdef * a - ($wdef - pdd) * b;
+  $: dealMagicDamage = (variance) =>
+    Math.pow(mob.matt, 2) * variance -
+    (($mdef + $int) / 4 + $str / 28 + $dex / 24 + $luk / 20) *
+      ($job == "magician" ? 1.2 : 1);
+
+  $: display = [
+    ["avoidability", avoid_mob],
+    ["physical dodge", dodge],
+    ["physical hit", hit],
+    ["magic dodge", mdodge],
+    ["magic hit", mhit],
+    ["pdd", pdd],
+    ["weapon damage (min)", dealWeaponDamage(0.008)],
+    ["weapon damage (max)", dealWeaponDamage(0.0085)],
+    ["magic damage (min)", dealMagicDamage(0.0075)],
+    ["magic damage (max)", dealMagicDamage(0.008)],
+  ].map(([key, value]) => [key, value % 1 === 0 ? value : value.toFixed(3)]);
+</script>
+
+<h2>Calculations</h2>
+
+<Table data={display} header={false} />
